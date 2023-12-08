813f0f8e7ba9c5489d6f82a2829687af0b6cfa7d
JanGaud <JanisGaudreault.jg@gmail.com>
2023-12-08 18:28:37 -0500

    searchbar works!!!

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,8 +1,14 @@
 <script>
+	import { createEventDispatcher } from 'svelte';
 	import { Search, Button } from 'flowbite-svelte';
 	import Icon from '@iconify/svelte';
 
+	const dispatch = createEventDispatcher();
+
 	let searchTerm = '';
+	/**
+	 * @type {never[]}
+	 */
 	let cocktails = [];
 
 	async function searchCocktail() {
@@ -18,39 +24,40 @@
 		try {
 			const response = await fetch(url, options);
 			const result = await response.json();
-			console.log(result);
-			cocktails = result.drinks;
+
+			if (result.drinks) {
+				cocktails = result.drinks;
+				dispatch('searchCompleted', { cocktails, found: true });
+			} else {
+				cocktails = [];
+				dispatch('searchCompleted', { cocktails, found: false });
+			}
 		} catch (error) {
 			console.error(error);
+			cocktails = [];
+			dispatch('searchCompleted', { cocktails, found: false });
 		}
 	}
 
-	function handleVoiceBtn() {
-		alert('You clicked the voice button');
-	}
 </script>
 
-<div class="-mx-2 sm:-mx-20 -my-10 bg-[#2f2f2f44] backdrop-blur-sm py-10 px-10">
-<form class="flex gap-2">
-	<Search
-		size="md"
-		class="flex gap-2 items-center shadow-inner focus:ring-main-green focus:border-spark-yellow"
-		placeholder="Faites une recherche"
-		bind:value={searchTerm}
-	>
-		<button type="button" on:click={handleVoiceBtn} class="outline-none">
-			<Icon icon="ic:outline-mic" class="w-4 h-4 me-2" />
-		</button>
-	</Search>
+<div class="-mx-2 sm:-mx-20 -my-10 bg-[#2f2f2f2a] backdrop-blur-sm py-10 px-2 sm:px-10">
+	<form class="flex gap-2" on:submit|preventDefault={searchCocktail}>
+		<Search
+			size="md"
+			class="flex gap-2 items-center shadow-inner focus:ring-main-green focus:border-spark-yellow"
+			placeholder="Faites une recherche"
+			bind:value={searchTerm}
+		/>
 
-	<Button
-		color="cyan"
-		size="sm"
-		class="bg-[#97ca4bc4] backdrop-blur-sm hover:bg-[#f6f930c0] focus:ring-1 focus:ring-main-green hover:text-brand-gray shadow-md transition-colors"
-		on:click={searchCocktail}
-	>
-		<Icon icon="ic:round-search" class="w-5 h-5 me-2 -ms-1" />
-		Rechercher
-	</Button>
-</form>
+		<Button
+			color="cyan"
+			size="sm"
+			class="bg-[#97ca4bc4] backdrop-blur-sm hover:bg-[#f6f930c0] focus:ring-1 focus:ring-main-green hover:text-brand-gray shadow-md transition-colors"
+			on:click={searchCocktail}
+		>
+			<Icon icon="ic:round-search" class="w-5 h-5 me-2 -ms-1" />
+			Rechercher
+		</Button>
+	</form>
 </div>
--- src/routes/populaires/+page.svelte
+++ src/routes/populaires/+page.svelte
@@ -1,60 +1,64 @@
 <script>
 	import { Card, Button, Toggle, ImagePlaceholder } from 'flowbite-svelte';
-	import {  } from 'flowbite-svelte';
-    import { onMount } from 'svelte';
+	import {} from 'flowbite-svelte';
+	import { onMount } from 'svelte';
 
-    /**
+	/**
 	 * @type {string | any[]}
 	 */
-     export let cocktails = [];
+	export let cocktails = [];
 
+	onMount(async () => {
+		try {
+			const url = 'https://the-cocktail-db.p.rapidapi.com/popular.php';
+			const response = await fetch(url, {
+				method: 'GET',
+				headers: {
+					'X-RapidAPI-Key': 'ded98c2facmsh419ce08fa864bb6p127517jsn2d81dbc7b359',
+					'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+				}
+			});
 
-    onMount(async () => {
-        try {
-            const url = 'https://the-cocktail-db.p.rapidapi.com/popular.php';
-            const response = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    'X-RapidAPI-Key': 'ded98c2facmsh419ce08fa864bb6p127517jsn2d81dbc7b359',
-                    'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com',
-                },
-            });
-
-            if (response.ok) {
-                const data = await response.json();
-                cocktails = data.drinks;
-            } else {
-                console.error('API response was not ok');
-            }
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    });
+			if (response.ok) {
+				const data = await response.json();
+				cocktails = data.drinks;
+			} else {
+				console.error('API response was not ok');
+			}
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		}
+	});
 </script>
 
-
-	<div class="grid grid-cols-1 md:grid-cols-2 gap-4 mx-auto">{#if cocktails.length > 0}
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4 mx-auto">
+	{#if cocktails.length > 0}
 		{#each cocktails as cocktail}
-            <Card color="none" img={cocktail.strDrinkThumb} horizontal class="bg-[#ffffff25] backdrop-blur-xl hover:bg-[#f6f93017] group md:h-56">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-brand-gray">{cocktail.strDrink}</h5>
-                <div class="mb-3 group-hover:hidden">
-                    <h6 class="text-base font-semibold text-brand-gray">Ingrédients:</h6>
-                    <ul class="list-disc list-inside text-brand-gray flex flex-wrap gap-4">
-                        {#each Array.from({ length: 15 }, (_, i) => i + 1) as ingredientIndex}
-                            {#if cocktail[`strIngredient${ingredientIndex}`]}
-                                <li class="text-sm">{cocktail[`strIngredient${ingredientIndex}`]}</li>
-                            {/if}
-                        {/each}
-                    </ul>
-                </div>
-                <div class="group-hover:block hidden overflow-y-auto">
-                    <p class="mb-3 text-sm text-brand-gray leading-tight">{cocktail.strInstructions}</p>
-                </div>
-            </Card>
+			<Card
+				color="none"
+				img={cocktail.strDrinkThumb}
+				horizontal
+				class="bg-[#ffffff25] backdrop-blur-xl hover:bg-[#f6f93017] group md:h-56"
+			>
+				<h5 class="mb-2 text-2xl font-bold tracking-tight text-brand-gray">{cocktail.strDrink}</h5>
+				<div class="mb-3 group-hover:hidden">
+					<h6 class="text-base font-semibold text-brand-gray">Ingrédients:</h6>
+					<ul class="list-disc list-inside text-brand-gray flex flex-wrap gap-4">
+						{#each Array.from({ length: 15 }, (_, i) => i + 1) as ingredientIndex}
+							{#if cocktail[`strIngredient${ingredientIndex}`]}
+								<li class="text-sm">{cocktail[`strIngredient${ingredientIndex}`]}</li>
+							{/if}
+						{/each}
+					</ul>
+				</div>
+				<div class="group-hover:block hidden h-40 overflow-y-auto">
+					<p class="mb-3 text-sm text-brand-gray leading-tight">{cocktail.strInstructions}</p>
+				</div>
+			</Card>
 		{/each}
-		{:else}
-			{#each Array(6) as _, i}
-				<ImagePlaceholder class="bg-[#ffffff25] backdrop-blur-xl shadow-lg pr-4" />
-			{/each}
-		{/if}
-	</div>
+	{:else}
+		{#each Array(6) as _, i}
+			<ImagePlaceholder class="bg-[#ffffff25] backdrop-blur-xl shadow-lg pr-4" />
+		{/each}
+	{/if}
+</div>
