1a82dd147ad08bdea5efd564b2e6357a60e866d1
JanGaud <JanisGaudreault.jg@gmail.com>
2023-12-08 17:45:15 -0500

    searchbar sends data in the console

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,8 +1,32 @@
 <script>
 	import { Search, Button } from 'flowbite-svelte';
 	import Icon from '@iconify/svelte';
+
+	let searchTerm = '';
+	let cocktails = [];
+
+	async function searchCocktail() {
+		const url = `https://the-cocktail-db.p.rapidapi.com/search.php?s=${searchTerm}`;
+		const options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': 'ded98c2facmsh419ce08fa864bb6p127517jsn2d81dbc7b359',
+				'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+			}
+		};
+
+		try {
+			const response = await fetch(url, options);
+			const result = await response.json();
+			console.log(result);
+			cocktails = result.drinks;
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
 	function handleVoiceBtn() {
-		alert('You clicked voice button');
+		alert('You clicked the voice button');
 	}
 </script>
 
@@ -11,12 +35,19 @@
 		size="md"
 		class="flex gap-2 items-center shadow-inner focus:ring-main-green focus:border-spark-yellow"
 		placeholder="Faites une recherche"
+		bind:value={searchTerm}
 	>
 		<button type="button" on:click={handleVoiceBtn} class="outline-none">
 			<Icon icon="ic:outline-mic" class="w-4 h-4 me-2" />
 		</button>
 	</Search>
-	<Button color="cyan" size="sm" class="bg-[#97ca4bc4] backdrop-blur-sm hover:bg-[#f6f930c0] hover:text-brand-gray shadow-md transition-colors">
+
+	<Button
+		color="cyan"
+		size="sm"
+		class="bg-[#97ca4bc4] backdrop-blur-sm hover:bg-[#f6f930c0] focus:ring-1 focus:ring-main-green hover:text-brand-gray shadow-md transition-colors"
+		on:click={searchCocktail}
+	>
 		<Icon icon="ic:round-search" class="w-5 h-5 me-2 -ms-1" />
 		Rechercher
 	</Button>
