96125132760a5c1971e7d12205688ab1ab719b7e
JanGaud <JanisGaudreault.jg@gmail.com>
2023-12-10 19:54:15 -0500

    card icon button change

--- src/components/CocktailCard.svelte
+++ src/components/CocktailCard.svelte
@@ -18,35 +18,39 @@
 	color="none"
 	img={cocktail.strDrinkThumb}
 	horizontal
-	class="relative bg-[#ffffff25] hover:bg-[#f6f93017] backdrop-blur-xl group flex flex-col w-full md:h-56 cursor-pointer"
->
+	class="bg-[#ffffff25] hover:bg-[#f6f93017] backdrop-blur-xl group md:h-64 mx-auto"
+><div>
 	<h5 class="mb-2 text-2xl font-bold tracking-tight text-brand-gray">
 		{cocktail.strDrink}
 	</h5>
-	<button
-		class="absolute p-2 border border-brand-gray rounded-full shadow-md bg-[#97ca4bc4] backdrop-blur-sm hover:bg-[#f6f930c0] text-brand-gray transition-colors top-1 right-1 w-10 h-10 flex items-center justify-center focus:outline-none active:outline-none"
-		on:click={toggleInstructions}
-	>
-		<Icon class="w-6 h-6" icon="icon-park:switch"  />
-	</button>
-	{#if !showInstructions}
-		<div>
-			<h6 class="text-base font-semibold text-brand-gray">Ingredients:</h6>
-			<ul class="list-disc list-inside text-brand-gray flex flex-wrap h-32 overflow-y-auto gap-4">
-				{#each Array.from({ length: 15 }, (_, i) => i + 1) as ingredientIndex}
-					{#if cocktail[`strIngredient${ingredientIndex}`]}
-						<li class="text-sm">
-							{cocktail[`strIngredient${ingredientIndex}`]} - {cocktail[
-								`strMeasure${ingredientIndex}`
-							]}
-						</li>
-					{/if}
-				{/each}
-			</ul>
-		</div>
-	{:else}
-		<div class="h-32 overflow-y-auto">
-			<p class="mb-3 text-sm text-brand-gray leading-tight">{cocktail.strInstructions}</p>
-		</div>
-	{/if}
+	<div>
+		{#if !showInstructions}
+			<div class="h-28 overflow-y-auto">
+				<h6 class="text-base font-semibold text-brand-gray">Ingredients:</h6>
+				<ul class="list-disc list-inside text-brand-gray flex flex-wrap gap-4">
+					{#each Array.from({ length: 15 }, (_, i) => i + 1) as ingredientIndex}
+						{#if cocktail[`strIngredient${ingredientIndex}`]}
+							<li class="text-sm">
+								{cocktail[`strIngredient${ingredientIndex}`]} - {cocktail[
+									`strMeasure${ingredientIndex}`
+								]}
+							</li>
+						{/if}
+					{/each}
+				</ul>
+			</div>
+		{:else}
+			<div class="h-28 overflow-y-auto">
+				<p class="mb-3 text-sm text-brand-gray leading-tight">{cocktail.strInstructions}</p>
+			</div>
+		{/if}
+	</div>
+	<div class="w-full h-fit mt-10">
+		<button
+			class="rounded-lg shadow-md bg-[#97ca4bc4] backdrop-blur-sm hover:bg-[#f6f930c0] text-brand-gray transition-colors w-full h-8 flex gap-2 items-center justify-center focus:outline-none active:outline-none py-2"
+			on:click={toggleInstructions}
+		>
+			Voir plus <Icon class="w-5 h-5" icon="ph:plus-bold"  />
+		</button>
+	</div>
 </Card>
