b95664528958fefa3c47ec665d289a95be583a96
JanGaud <JanisGaudreault.jg@gmail.com>
2026-02-09 15:55:23 -0500

    video cocktail banner fix

--- src/components/VideoBanner.svelte
+++ src/components/VideoBanner.svelte
@@ -1,9 +1,13 @@
 <script>
-	export let gifSrc = './video/CocktailBanner.gif';
+	export let gifSrc = '/video/CocktailBanner.gif';
 </script>
 
 <div class="flex justify-center">
 	<div class="w-[600px] h-auto relative">
-		<img class="w-full h-full object-cover drop-shadow-2xl" src={gifSrc} alt="Cocktail Banner" />
+		<img
+			class="w-full h-full object-cover drop-shadow-2xl"
+			src={gifSrc}
+			alt="Cocktail Banner"
+		/>
 	</div>
 </div>
